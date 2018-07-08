d3e8b00b41e98acca33de5f72f82231c8a44e1ef
link375 <https://github.com/link375>
2018-07-08 14:16:06 -0600

    adding the template to the correct directory

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -1,11 +1,18 @@
 """Defines url patterns for learning_logs."""
 
+# import path to allow use to use path mapping
 from django.urls import path
 
+# import the view - or templates
 from . import views
 
+# define the name for this app
+# we can use this as a namespace
 app_name = 'learning_logs'
+
+# urls that should come to this app
 urlpatterns = [
     # Home page.
+    # url/pattern, templateLocation, nameOfURL
     path('', views.index, name='index'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -4,4 +4,5 @@ from django.shortcuts import render
 
 def index(request):
     """The home page for Learning Log"""
+    # return an html template
     return render(request, 'learning_logs/index.html')

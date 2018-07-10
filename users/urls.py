b86b91238733f29660647f67f9892c18e2f90922
link375 <https://github.com/link375>
2018-07-10 15:02:44 -0600

    adding url and template for the login page

--- users/urls.py
+++ users/urls.py
@@ -8,4 +8,6 @@ from . import views
 app_name = 'users'
 urlpatterns = [
     # login page
+    # skip the views and render the default django login page
+    path('login/', login, {'template_name': 'users/login.html'}, name='login')
 ]

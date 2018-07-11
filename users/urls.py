38627eb963737de96303b12f32ec093fbebc19e4
link375 <17466655+link375@users.noreply.github.com>
2018-07-10 21:35:08 -0600

    missing login view in django

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 """ Defines URL patterns for users """
 
 from django.urls import path
-from django.contrib.auth.views import login
+from django.contrib.auth.views import auth_login
 
 from . import views
 
@@ -9,7 +9,8 @@ app_name = 'users'
 urlpatterns = [
     # login page
     # skip the views and render the default django login page
+    # use the template from the defined path
     path('login/',
-         login, {'template_name': 'users/login.html'},
+         auth_login, {'template_name': 'users/login.html'},
          name='login')
 ]
